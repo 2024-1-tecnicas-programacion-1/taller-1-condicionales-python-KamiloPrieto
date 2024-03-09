1cf0bc84fb7d1817683783936f3d67d7a2364955
Camilo Prieto <juanc.prietom@konradlorenz.edu.co>
2024-03-09 17:38:33 -0500

    Entrega final División
    Se realizo el código para que el programa hiciera lo que se le pedía y se agregaron mas casos de prueba para comprobar que el programa funcionara correctamente.

--- tests/test.division.py
+++ tests/test.division.py
@@ -12,15 +12,33 @@ sys.path.append(str(root_path))
 from src.division import evaluar
 
 class TestDivision(unittest.TestCase):
-    def testDivisionExacta(self):
-        valor_esperado = "La división es exacta. \n" \
+    def testDivisionNoExacta(self):
+        valor_esperado = "La división no es exacta. \n" \
                          "Cociente: 2\n" \
                          "Residuo: 4"
         valor_actual = evaluar(14, 5)
         self.assertEqual(valor_esperado, valor_actual)
     
-    # TODO: Agrega tus otros casos de prueba aquí
+    def testDivisionExacta(self):
+        valor_esperado = "La división es exacta. \n" \
+                         "Cociente: 10\n" \
+                         "Residuo: 0"
+        valor_actual = evaluar(100, 10)
+        self.assertEqual(valor_esperado, valor_actual)
     
+    def testDivisionExactaDos(self):
+        valor_esperado = "La división es exacta. \n" \
+                         "Cociente: 24\n" \
+                         "Residuo: 0"
+        valor_actual = evaluar(48, 2)
+        self.assertEqual(valor_esperado, valor_actual)
+
+    def testDivisionNoExactaDos(self):
+        valor_esperado = "La división no es exacta. \n" \
+                         "Cociente: 38\n" \
+                         "Residuo: 1"
+        valor_actual = evaluar(77, 2)
+        self.assertEqual(valor_esperado, valor_actual)
 
 if __name__ == '__main__':
     unittest.main()
